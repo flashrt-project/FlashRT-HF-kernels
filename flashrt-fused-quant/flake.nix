992e34a27ccae454975a6988a1bece249dd835c3
LiangSu8899 <1342163629@qq.com>
2026-06-05 04:26:36 -0400

    Pin PR619 kernel builder for v1 publish

--- flashrt-fused-quant/flake.nix
+++ flashrt-fused-quant/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for FlashRT fused quantization kernels";
 
   inputs = {
-    kernel-builder.url = "github:huggingface/kernels";
+    kernel-builder.url = "github:huggingface/kernels/final-triton-hashes";
   };
 
   outputs =
