f87f473286e02d6f886f4cea054409ad230a74b5
LiangSu8899 <1342163629@qq.com>
2026-07-12 16:37:36 -0400

    Adopt current kernel builder flake API

--- int4-blackwell/flake.nix
+++ int4-blackwell/flake.nix
@@ -1,6 +1,12 @@
 {
+  description = "Flake for FlashRT SM120 INT4 primitives";
+
   inputs.kernel-builder.url = "github:huggingface/kernels";
 
-  outputs = { self, kernel-builder }:
-    kernel-builder.lib.genFlakeOutputs ./.;
+  outputs =
+    { self, kernel-builder }:
+    kernel-builder.lib.genKernelFlakeOutputs {
+      inherit self;
+      path = ./.;
+    };
 }
