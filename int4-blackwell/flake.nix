4c27653c1a6356da4ed3fda84553ca54b3433d7e
LiangSu8899 <1342163629@qq.com>
2026-07-12 16:35:52 -0400

    Fix INT4 kernel builder flake reference

--- int4-blackwell/flake.nix
+++ int4-blackwell/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.kernel-builder.url = "github:huggingface/kernels/1";
+  inputs.kernel-builder.url = "github:huggingface/kernels";
 
   outputs = { self, kernel-builder }:
     kernel-builder.lib.genFlakeOutputs ./.;
