ab70328725b6c0d098353927c267de6a8147eb36
LiangSu8899 <1342163629@qq.com>
2026-08-11 06:53:32 -0400

    Refresh masked MHA builder lock

--- masked-mha-runtime/flake.nix
+++ masked-mha-runtime/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Flake for FlashRT masked MHA runtime kernels";
-  inputs.kernel-builder.url = "github:huggingface/kernels/870e825d881664e39f9287a27a74ef63ff3c545e";
+  inputs.kernel-builder.url = "github:huggingface/kernels/2c40e10d3da1392245ee81bd05b55fb3103b2b34";
   outputs = { self, kernel-builder }:
     kernel-builder.lib.genKernelFlakeOutputs { inherit self; path = ./.; };
 }
