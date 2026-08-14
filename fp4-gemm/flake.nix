3cf453b28a019b4703dc3a8092eb6badc8f2b18c
LiangSu8899 <1342163629@qq.com>
2026-08-14 06:03:40 -0400

    Add FP4 Torch 2.11 compatibility build pin [skip ci]

--- fp4-gemm/flake.nix
+++ fp4-gemm/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Includes the CUTLASS 4.4 dependency mapping required by the SM110 path.
-    kernel-builder.url = "github:huggingface/kernels";
+    kernel-builder.url = "github:flashrt-project/kernels/flashrt-torch211-cutlass44";
   };
 
   outputs =
