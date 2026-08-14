646d5e22f9a6bfb007b8104dad3d4a1d03bb76f4
LiangSu8899 <1342163629@qq.com>
2026-08-14 06:04:21 -0400

    Restore FP4 current HF builder pin [skip ci]

--- fp4-gemm/flake.nix
+++ fp4-gemm/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Includes the CUTLASS 4.4 dependency mapping required by the SM110 path.
-    kernel-builder.url = "github:flashrt-project/kernels/flashrt-torch211-cutlass44";
+    kernel-builder.url = "github:huggingface/kernels";
   };
 
   outputs =
